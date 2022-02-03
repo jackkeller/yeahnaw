69cfbf61198c719e1792c80b99c725c648d2fb66
Jack Keller <jack.keller@wwt.com>
2022-02-03 17:09:48 -0600

    initial commit with basic layout

---
+++ notes.md
@@ -0,0 +1,15 @@
+## Components
+
+- Question?
+- Voting Button(s)
+- Timer
+- Responses
+- Tally (total responses)
+
+## Functions
+
+- Voting Function
+
+## Views Routes
+
+Index: input for question
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,21 @@
+<script>
+  import '../styles/app.css'
+</script>
+
+<header>
+  <div class="container">
+    <h1>Do you want free pizza on Fridays?</h1>
+  </div>
+</header>
+
+<main>
+  <div class="container">
+    <slot />
+  </div>
+</main>
+
+<footer>
+  <div class="container">
+    I am a footer
+  </div>
+</footer>
