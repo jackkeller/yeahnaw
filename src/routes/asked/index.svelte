1b82843fecdd561f8e111cac27437057f1a56c07
Jack Keller <jack.keller@wwt.com>
2022-02-17 17:24:50 -0600

    Playing around with calculations, and animating thumbs

--- src/routes/asked/index.svelte
+++ src/routes/asked/index.svelte
@@ -1,5 +1,6 @@
 <script>
 	import Counter from '@components/Counter.svelte'
+  import Thumbs from '@components/Thumbs.svelte'
 	import { Question, Yeah, Naw } from '@app/store.js'
 
 	export let TotalCount = 0,
@@ -32,7 +33,7 @@
 
 	const voting = () => {
 		const min = 250,
-			max = 1750
+			max = 1250
 		const random = Math.floor(Math.random() * (max - min + 1)) + min
 
 		if (TotalCount < 50) {
@@ -52,3 +53,5 @@
 </svelte:head>
 
 <Counter />
+
+<Thumbs yeah={yeah} naw={naw} />
